128a8fcc66b46212d2662b602e60e484641b1f14
diegohabits <diego@habits.ai>
2020-03-26 12:15:29 -0600

    fix Activity

--- controllers/activity.js
+++ controllers/activity.js
@@ -85,17 +85,17 @@ function saveActivity(req, res){
 
    let mySave = new activity()
     mySave.active = true
-    mySave.title = actividad.title
-    mySave.rules = actividad.rules
-    mySave.type = actividad.type
-    mySave.points = actividad.points
-    mySave.level = actividad.level
-    mySave.extra = actividad.extra
-    mySave.pillar = actividad.pillar
-    mySave.duration = actividad.duration
+    mySave.title = param.title
+    mySave.rules = param.rules
+    mySave.type = param.type
+    mySave.points = param.points
+    mySave.level = param.level
+    mySave.extra = param.extra
+    mySave.pillar = param.pillar
+    mySave.duration = param.duration
     mySave.save((err, activeSaved) => {
         if(!err){
-            res.status(200).send({actividad: arraySaved})
+            res.status(200).send({actividad: activeSaved})
         }
     });
 
